882c7823d2f3c85199fe7f2b0c5b7e11b3c6456d
okayzach <znortyy@gmail.com>
2022-05-09 18:49:45 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.use(express.static('public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to the bread app')
@@ -13,6 +19,11 @@ app.get('/', (req, res) => {
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 // LISTEN
 app.listen(PORT, () => {
